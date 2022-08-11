598437ec2a1b590a100fd82402e6c3d98faee4bf
Harshit Swarnkar <96280175+Rew0611@users.noreply.github.com>
2022-08-11 22:07:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import "./App.scss";
+import Slider from "./components/slider/Slider";
+
+function App() {
+  return (
+    <div>
+      <Slider />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/slider/Slider.js
@@ -0,0 +1,126 @@
+import { useState, useEffect } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { sliderData } from "../../slider-data";
+import "./Slider.scss";
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideLength = sliderData.length;
+
+  const autoScroll = true;
+  let slideInterval;
+  let intervalTime = 5000;
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+    console.log("next");
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+    console.log("prev");
+  };
+
+  function auto() {
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+
+  useEffect(() => {
+    setCurrentSlide(0);
+  }, []);
+
+  useEffect(() => {
+    if (autoScroll) {
+      auto();
+    }
+    return () => clearInterval(slideInterval);
+  }, [currentSlide]);
+
+  return (
+    <>
+    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+   <div className="flex items-center flex-shrink-0 text-white mr-6">
+     <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+     <span className="font-semibold text-xl tracking-tight">Image Carousel</span>
+   </div>
+   <div className="block lg:hidden">
+     <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+       <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+     </button>
+   </div>
+   <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+     <div className="text-sm lg:flex-grow">
+       <a href="#carousel" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+         Carousel
+       </a>
+       <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+         Images
+       </a>
+     </div>
+   </div>
+ </nav>
+ <div className="slider" id='carousel'>
+      <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide} />
+      <AiOutlineArrowRight className="arrow next" onClick={nextSlide} />
+      {sliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === currentSlide ? "slide current" : "slide"}
+            key={index}
+          >
+            {index === currentSlide && (
+              <div>
+                <img src={slide.image} alt="slide" className="image" />
+                <div className="content">
+                  <h2>{slide.heading}</h2>
+                  <p>{slide.desc}</p>
+                  <hr />
+                  <button className="--btn --btn-primary">Get Started</button>
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+    
+    <section className="overflow-hidden text-gray-700">
+  <div className="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+    <div className="flex flex-wrap -m-1 md:-m-2">
+      <div className="flex flex-wrap w-1/2">
+        <div className="w-1/2 p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+            src="https://i.ibb.co/58Mq6Mb/slide1.jpg" />
+        </div>
+        <div className="w-1/2 p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
+        </div>
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+        </div>
+      </div>
+      <div className="flex flex-wrap w-1/2">
+        <div className="w-full p-1 md:p-2">
+          <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
+        </div>
+        <div className="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
+        </div>
+        <div className="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </>
+    
+  );
+};
+
+export default Slider;
