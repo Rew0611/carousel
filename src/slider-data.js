cf2b2071a4e39105ef7da90442642392a06f7b9f
Harshit Swarnkar <96280175+Rew0611@users.noreply.github.com>
2022-08-11 22:47:17 +0530

    Update slider-data.js

--- src/slider-data.js
+++ src/slider-data.js
@@ -1,13 +1,13 @@
 export const sliderData = [
     {
-      image: "https://i.ibb.co/58Mq6Mb/slide1.jpg",
-      heading: "Slide One",
-      desc: "This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+      image: "https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      heading: "Beautiful View of Moraine Lake",
+      desc: "oraine Lake is a glacially fed lake in Banff National Park.",
     },
     {
-      image: "https://i.ibb.co/8gwwd4Q/slide2.jpg",
-      heading: "Slide Two",
-      desc: "This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+      image: "https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&w=600",
+      heading: "Brown Mountains",
+      desc: "Brown Mountain is a low-lying ridge, approximately 1.5 miles (2.4 km) long, in the Pisgah National Forest near Morganton.",
     },
     {
       image: "https://i.ibb.co/8r7WYJh/slide3.jpg",
